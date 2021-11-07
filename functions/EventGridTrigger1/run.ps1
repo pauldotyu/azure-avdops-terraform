a2915e44cdd8ec47734f4129cebbe7ef37590b3f
Paul Yu <pauldotyu@gmail.com>
2021-11-07 13:47:28 -0800

    Add function

---
+++ functions/EventGridTrigger1/run.ps1
@@ -0,0 +1,27 @@
+param($eventGridEvent, $TriggerMetadata)
+
+# Authenticate using managed identity
+Connect-AzAccount -Identity
+
+# Make sure to pass hashtables to Out-String so they're logged correctly
+$eventGridEvent | Out-String | Write-Host
+
+Write-Host $eventGridEvent.Subject
+
+$vmResource = Get-AzResource -Id $eventGridEvent.Subject
+
+Write-Host $vmResource.ResourceGroupName
+
+$vm = Get-AzVM -Name $vmResource.Name -ResourceGroupName $vmResource.ResourceGroupName
+
+Write-Host $vm.Id
+
+#Update-AzVM -VM $vm -IdentityType SystemAssigned -ResourceGroupName $vmResource.ResourceGroupName
+
+#$vm = Get-AzVM -Name $vmResource.Name -ResourceGroupName $vmResource.ResourceGroupName
+#$id = $vm.Identity.PrincipalId
+
+#Write-Host $id
+
+#$groupId = (Get-AzADGroup -DisplayName $vmResource.ResourceGroupName).ID
+#Add-AzADGroupMember -MemberObjectId $id -TargetGroupObjectId $groupId
