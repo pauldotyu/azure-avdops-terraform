d7d473defd9499d3524b23f02518ec492369b64d
Paul Yu <pauldotyu@gmail.com>
2021-11-07 16:27:55 -0800

    Update function app

--- function/AIBIdentity/run.ps1
+++ function/AIBIdentity/run.ps1
@@ -1,5 +1,8 @@
 param($eventGridEvent, $TriggerMetadata)
 
+# Authenticate using managed identity
+Connect-AzAccount -Identity
+
 # Make sure to pass hashtables to Out-String so they're logged correctly
 $eventGridEvent | Out-String | Write-Host
 
@@ -13,12 +16,12 @@ $vm = Get-AzVM -Name $vmResource.Name -ResourceGroupName $vmResource.ResourceGro
 
 Write-Host $vm.Id
 
-Update-AzVM -VM $vm -IdentityType SystemAssigned -ResourceGroupName $vmResource.ResourceGroupName
+#Update-AzVM -VM $vm -IdentityType SystemAssigned -ResourceGroupName $vmResource.ResourceGroupName
 
-$vm = Get-AzVM -Name $vmResource.Name -ResourceGroupName $vmResource.ResourceGroupName
-$id = $vm.Identity.PrincipalId
+#$vm = Get-AzVM -Name $vmResource.Name -ResourceGroupName $vmResource.ResourceGroupName
+#$id = $vm.Identity.PrincipalId
 
-Write-Host $id
+#Write-Host $id
 
 #$groupId = (Get-AzADGroup -DisplayName $vmResource.ResourceGroupName).ID
 #Add-AzADGroupMember -MemberObjectId $id -TargetGroupObjectId $groupId
